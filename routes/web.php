b68bed38d7f31767fb6fceb25a2c369d83eb3186
Todor Anev <todoranev99gmail.com>
2021-04-21 21:47:34 +0300

    Admin Panel and Admin Logout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,10 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index'); // when we login redirect to 'dashboard' page in view
 })->name('dashboard');
+
+
+// Admin Logout
+
+Route::get('/admin/logout', [AdminController::class, 'Logout'])->name('admin.logout');
