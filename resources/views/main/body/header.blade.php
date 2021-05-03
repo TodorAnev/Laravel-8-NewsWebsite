18db767fa2ab12568424f648469048b3e187150b
Todor Anev <todoranev99gmail.com>
2021-05-04 01:17:00 +0300

    Category and SubCategory Frontend

--- resources/views/main/body/header.blade.php
+++ resources/views/main/body/header.blade.php
@@ -1,3 +1,10 @@
+@php
+
+	$category = DB::table('categories')->orderBy('id', 'asc')->get();
+
+
+@endphp
+
 <!-- header-start -->
 	<section class="hdr_section">
 		<div class="container-fluid">			
@@ -25,29 +32,25 @@
 				<div id="navbarCollapse" class="collapse navbar-collapse">
 					<ul class="nav navbar-nav">
 						<li><a href="#">HOME</a></li>
-						<li><a href="#">BUSINESS</a></li>
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown">WORLD <b class="caret"></b></a>
-							<ul class="dropdown-menu">
-								<li><a href="#">USA</a></li>
-								<li><a href="#">UK</a></li>
-							</ul>
-							</li>
+
+			@foreach($category as $row)
+
+			@php 
+
+				$subcategory = DB::table('subcategories')->where('category_id', $row->id)->get();
+
+			@endphp
+	<li class="dropdown">
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown">{{  $row->category_en }}<b class="caret"></b></a>
+	<ul class="dropdown-menu">
+		@foreach($subcategory as $row)
+		<li><a href="#">{{  $row->subcategory_en }}</a></li>
+		@endforeach
+	</ul>
+	</li>
 						 
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">FASHION<b class="caret"></b></a>
-							<ul class="dropdown-menu">
-								<li><a href="#">Mans FASHION</a></li>
-								<li><a href="#">Woman FASHION</a></li>
-							</ul>
-						</li>
-						<li><a href="#">POLITICS </a></li>
-						<li><a href="#">SPORTS</a></li>
-					
-						
-						<li><a href="#" target="_blank">HEALTH</a></li>
-						<li><a href="#" target="_blank">VIDEOS</a></li>
-						 
+		@endforeach				
+					 
 				</div>
 			</nav>											
 		</div>
